9492df51284e70f78820d90ce9b328d231495147
Katy Deng <deng2017.xq@gmail.com>
2018-11-09 14:56:30 -0800

    connect mongodb

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose'); 
+
 
 const app = express(); 
-
+//DB config 
+const db = require('./config/key').mongoURI;
+//connect to mongoDB
+mongoose
+  .connect(db)
+  .then(()=>{console.log('MongoDB Connected')})
+  .catch(err => console.log(err));
+//setting up express server
 app.get('/', (req, res)=>res.send('Hello'))
 
 const port = process.env.PORT || 5000;
