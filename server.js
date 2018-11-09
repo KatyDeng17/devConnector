a94260cc2d5f64d7ee44fa1ef54c2ac9b3249038
Katy Deng <deng2017.xq@gmail.com>
2018-11-09 15:33:05 -0800

    setup route files with express router

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 
+//4. setting up the routes to use
+const users = require('./routes/api/usersAccount');
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+
 
 const app = express(); 
-//DB config 
+
+
+//2 .DB config 
 const db = require('./config/key').mongoURI;
-//connect to mongoDB
+
+//3. connect to mongoDB
 mongoose
   .connect(db)
   .then(()=>{console.log('MongoDB Connected')})
   .catch(err => console.log(err));
-//setting up express server
+  
+//1. setting up express server
 app.get('/', (req, res)=>res.send('Hello'))
+
+//5. use Routes 
+app.use('/api/users', users)
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
